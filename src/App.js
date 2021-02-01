c020fd28ea71d7ada0e7eba24e37e7ac4ce57eed
unknown <yher25@gmail.com>
2021-02-01 16:11:04 +0800

    update landing page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import HeaderBar from "./components/HeaderBar";
 import CreateCrop from "./components/CreateCrop";
 import GetCrops from "./components/GetCrops";
 import Container from '@material-ui/core/Container';
-
+import Card from "./components/Card";
 function App() {
   return (
     <Router>
         <HeaderBar />
+        <div className="wrapper">
+        </div>
         <Container>
-          <h1>Contract URL : </h1>
-          <a target="_blank" rel="noreferrer" href="https://rinkeby.etherscan.io/address/0x0e0e3188F1Cc8DAcBE1b889EFEA1E393775b32ED" style={{color : "#000"}}>0x0e0e3188F1Cc8DAcBE1b889EFEA1E393775b32ED</a>
+          {/* <h1>Contract URL : </h1>
+          <a target="_blank" rel="noreferrer" href="https://rinkeby.etherscan.io/address/0x0e0e3188F1Cc8DAcBE1b889EFEA1E393775b32ED" style={{color : "#000"}}>0x0e0e3188F1Cc8DAcBE1b889EFEA1E393775b32ED</a> */}
           <Switch>
             <Route exact path="/" />
             <Route path={`/farm/create`} component={CreateCrop} />
             <Route path={`/farm/get`} component={GetCrops} />
           </Switch>
+          <Card />
+          
         </Container>
+        
     </Router>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import SvgIcon from "../components/SvgIcon";
+const useStyles = makeStyles({
+    root: {
+      display : "flex",
+      justifyContent : "space-between",
+      alignItems : "center",
+      marginTop : "3em",
+      marginBottom : "3em",
+      
+    },
+    card : {
+        display : "flex",
+        justifyContent : "center",
+        alignItems : "center",
+        flexDirection : "column",
+        width : "30%",
+        background : "#3b956c",
+        height : "40vh",
+        color : "#fff"
+    },
+    svg : {
+        // margin : "1em",
+        width : "150px"
+    }
+  });
+const Card = () => {
+    const classes = useStyles();
+    return(
+        <div className={classes.root}>
+            <div className={classes.card}>
+                <SvgIcon.Farmer className={classes.svg}/>
+                <Typography variant="h5" gutterBottom>
+                    Lorem ipsum dolor sit amet.
+                </Typography>
+            </div>
+            <div className={classes.card}>
+                <SvgIcon.Ecology className={classes.svg}/>
+                <Typography variant="h5" gutterBottom>
+                    Lorem ipsum dolor sit amet.
+                </Typography>
+            </div>
+            <div className={classes.card}>
+                <SvgIcon.Sprout className={classes.svg}/>
+                <Typography variant="h5" gutterBottom>
+                    Lorem ipsum dolor sit amet.
+                </Typography>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
