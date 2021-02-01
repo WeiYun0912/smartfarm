f746e51c24a4a267227e13636ccec26947ea2fe7
ChunWeiYeah <yher25@gmail.com>
2021-02-01 20:40:55 +0800

    update index page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HeaderBar from "./components/HeaderBar";
 
 import CreateCrop from "./components/CreateCrop";
 import GetCrops from "./components/GetCrops";
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 import Card from "./components/Card";
+import Home from "./components/Home";
 function App() {
   return (
     <Router>
-        <HeaderBar />
-        <div className="wrapper">
-        </div>
-        <Container>
-          {/* <h1>Contract URL : </h1>
+      <HeaderBar />
+      <Route exact path="/" component={Home} />
+      <Container>
+        {/* <h1>Contract URL : </h1>
           <a target="_blank" rel="noreferrer" href="https://rinkeby.etherscan.io/address/0x0e0e3188F1Cc8DAcBE1b889EFEA1E393775b32ED" style={{color : "#000"}}>0x0e0e3188F1Cc8DAcBE1b889EFEA1E393775b32ED</a> */}
-          <Switch>
-            <Route exact path="/" />
-            <Route path={`/farm/create`} component={CreateCrop} />
-            <Route path={`/farm/get`} component={GetCrops} />
-          </Switch>
-          <Card />
-          
-        </Container>
-        
+        <Switch>
+          <Route path={`/farm/create`} component={CreateCrop} />
+          <Route path={`/farm/get`} component={GetCrops} />
+        </Switch>
+      </Container>
     </Router>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import Card from "./Card";
+const Home = () => {
+  return (
+    <>
+      <div className="wrapper"></div>
+      <Card />
+    </>
+  );
+};
+
+export default Home;
